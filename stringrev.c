43073b40df6ea9c669f02e28f4d5a7aedc4c875f
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2026-02-21 15:00:50 +0530

    Refactor string reversal with debug option
    Refactor string reversal program to use a predefined string and add debug prints.

--- stringrev.c
+++ stringrev.c
@@ -1,20 +1,26 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 #include<string.h>
-
+#define debug 1
 int main() {
     // Write C code here
-  char s[20];
-  gets(s);
+  char s[20]="hello";
+  //gets(s);
   char n=strlen(s);
   char start=0,end=n-1;
   while(start<end){
       s[start]^=s[end];
+      #if debug
       printf("\nstart1=%c",s[start]);
+      #endif
       s[end]^=s[start];
+      #if debug
       printf("\tend1=%c",s[end]);
+      #endif
       s[start]^=s[end];
+      #if debug
       printf("\tstart2=%c",s[start]);
+      #endif
       start++;
       end--;
   }
